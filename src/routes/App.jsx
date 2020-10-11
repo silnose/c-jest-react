a1f9d9000c3097f4dc682e2902bcaae910a5cf54
silnose <murgosilvana@gmail.com>
2020-10-11 11:47:39 -0300

    update baseNameUrl for github pages config

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -6,11 +6,11 @@ import Layout from '../components/Layout';
 import NotFound from '../containers/NotFound';
 
 const App = () => (
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <Layout>
       <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/checkout" component={Checkout} />
+        <Route exact path='/' component={Home} />
+        <Route exact path='/checkout' component={Checkout} />
         <Route component={NotFound} />
       </Switch>
     </Layout>
