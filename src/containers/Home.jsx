e1ea0756a14e4bd45301184b6c6db43768870e20
silnose <murgosilvana@gmail.com>
2020-10-11 23:50:03 -0300

    WIP

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import Products from '../components/Products';
 
-const Home = () => <Products />;
+const Home = () => {
+  console.log(process.env.NODE_ENV);
+  return <Products />;
+};
 
 export default Home;
