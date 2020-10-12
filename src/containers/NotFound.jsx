1edce753efd44ec324bc11482b9c226174cde5a2
silnose <murgosilvana@gmail.com>
2020-10-12 00:04:54 -0300

    WIP

--- src/containers/NotFound.jsx
+++ src/containers/NotFound.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
-const NotFound = () => (
-  <h1>NotFound</h1>
-);
+const NotFound = () => {
+  console.log(process.env.NODE_ENV);
+  console.log(PUBLIC_URL);
+  return <h1>NotFound</h1>;
+};
 
 export default NotFound;
